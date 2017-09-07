58679558ba6a04b741b8df457587488ece376b0d
kreamacbea <kreamacbea@gmail.com>
2017-09-07 22:20:20 +0200

    Add styling homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root to: "pages#home"
 
   devise_for :users
 
   resources :users, only: [:show]
   resources :profiles, only: [:new, :edit, :create, :update]
-
+  resources :artist, only: [:show, :new, :edit, :create, :update]
+  resources :songs, except: [:destroy]
+  resources :photos, only: [:destroy]
 end
