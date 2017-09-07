66c3c38ce33931910d108ea36a16f825259c2243
kreamacbea <kreamacbea@gmail.com>
2017-09-07 20:53:27 +0200

    Update devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root "pages#home"
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
 
-  # devise_for :users
+  resources :users, only: [:show]
+  resources :profiles, only: [:new, :edit, :create, :update]
+
 end
