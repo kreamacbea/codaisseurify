583eeda9b8f66909a41fe4f81df2a79283b3dcac
kreamacbea <kreamacbea@gmail.com>
2017-09-09 12:59:10 +0200

    Add model Artist and Song, seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ARTISTS
+artist1 = Artist.create!(name: "Odesza", image_url: "")
+artist2 = Artist.create!(name: "Moloko", image_url: "")
+artist3 = Artist.create!(name: "Goldfrapp", image_url: "")
+artist4 = Artist.create!(name: "Röyksopp", image_url: "")
+artist5 = Artist.create!(name: "Bat For Lashes", image_url: "")
+artist6 = Artist.create!(name: "Air", image_url: "")
+
+# SONGS
+Song.create!([
+  { title: "Intro", length: "1:03" },
+  { title: "A Moment Apart", length: "3:54" },
+  { title: "Higher Ground (feat. Naomi Wild)", length: "3:35" },
+  { title: "Boy", length: "3:03" },
+  { title: "Line Of Sight (feat. WYNNE & Mansionair)", length: "3:57" },
+  { title: "Late Night", length: "3:48" },
+  { title: "Across The Room (feat. Leon Bridges)", length: "4:43" },
+  { title: "Meridian", length: "3:55" },
+  { title: "Everything At Your Feet (feat. The Chamanas)", length: "3:28" },
+  { title: "Just A Memory (feat. Regina Spektor)", length: "3:56" },
+  { title: "Divide (feat. Kelsey Bulkin)", length: "4:01" },
+  { title: "Thin Floors And Tall Ceilings", length: "2:58", },
+  { title: "La Ciudad", length: "4:31" },
+  { title: "Falls (feat. Sasha Sloan)", length: "3:52" },
+  { title: "Show Me", length: "3:38" },
+  { title: "Corners Of The Earth (feat. RY X)", length: "5:05" },
+  { title: "Radio Moscow", length: "0:25" },
+  { title: "Pure Pleasure Seeker (feat. Todd Edwards)", length: "6:31" },
+  { title: "Absent Minded Friends", length: "4:45" },
+  { title: "Indigo", length: "5:36" },
+  { title: "Being Is Bewildering", length: "4:07" },
+  { title: "Remain the Same", length: "3:39" },
+  { title: "A Drop In the Ocean", length: "1:58" },
+  { title: "Dumb Inc.", length: "4:26" },
+  { title: "The Time Is Now", length: "5:18" },
+  { title: "Mother", length: "4:45" },
+  { title: "It's Your Problem", length: "1:05" },
+  { title: "It's Nothing", length: "5:12" },
+  { title: "Bingo Massacre", length: "0:25" },
+  { title: "Somebody Somewhere", length: "5:42" },
+  { title: "Just You and Me Dancing", length: "1:17" },
+  { title: "If You Have a Cross to Bear You May As Well Use It As a Crutch", length: "2:56" },
+  { title: "Keep Stepping", length: "0:21" },
+  { title: "Sing It Back (Boris Dlugosch Musical Mix)", length: "9:20" },
+])
